ae3e9ce37df5834fefc723f01c798426413ad80b
Iman Hosseini-Shakib <48807629+imshakib@users.noreply.github.com>
2021-10-06 14:37:16 -0400

    Update 06-Return_Periods.R

--- 06-Return_Periods.R
+++ 06-Return_Periods.R
@@ -1,39 +1,3 @@
-##==============================================================================
-##
-## Script to compare return levels with and without uncertainty quantification
-##
-## Authors: Mahkameh Zarekarizi (mahkameh.zare@gmail.com)
-##==============================================================================
-## Copyright 2019 Mahkameh Zarekarizi
-## This file is free software: you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation, either version 3 of the License, or
-## (at your option) any later version.
-##
-## This file is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-##
-## You should have received a copy of the GNU General Public License
-## along with this file.  If not, see <http://www.gnu.org/licenses/>.
-##==============================================================================
-## Instructions to run:
-## 1. If you have not already done so, change the working directory to the main
-##    folder (Zarekarizi-Home-Elevation)
-##    To do so:
-##      1. If on RStudio, open the README.md file. Then on the menu bar, go to
-##         Session-->Set Working Directory-->To Source File Location
-##      2. If on RStudio, on the lower right box, open "Zarekarizi-Home-Elevation"
-##         Then, click on More --> Set as Working Directory
-##      3. On console, type the following command:
-##         setwd("~/.../../../../Zarekarizi-Home-Elevation")
-## 2. To Run:
-##      1. Click on Source button or, on console, type: Source("../../....R")
-## 3. Outputs:
-##      1. output includes a plot that compares return levels with and without uncertainty
-##==============================================================================
-
 # Global variables
 wd<-getwd()
 setwd(wd)
